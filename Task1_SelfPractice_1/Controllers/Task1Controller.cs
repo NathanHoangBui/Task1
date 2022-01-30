cef81b1c943c402531279352f5ba313ec3699626
hbui6373@uni.sydney.edu.au <hbui6373@uni.sydney.edu.au>
2022-01-30 13:59:13 +1100

    Page Update

---
+++ Task1_SelfPractice_1/Controllers/Task1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task1_SelfPractice_1.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Task1()
+        {
+            return View();
+        }
+    }
+}
